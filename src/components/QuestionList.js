66cd523f2fb072dabeb41aff8ad34d5ef825a733
Racheal <rachealnjugush@gmail.com>
2024-11-15 16:49:37 +0300

    passed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,50 +1,21 @@
-import React, { useState, useEffect } from "react";
-import NewQuestionForm from "./QuestionForm";
-import QuestionItem from "./QuestionItem"
+import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:4000/questions")
-      .then((response) => response.json())
-      .then((data) => setQuestions(data))
-      .catch((error) => console.error("Error fetching questions:", error));
-  }, []);
-
-  function handleAddQuestion(newQuestion) {
-    setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
-  }
-
-  function handleDelete(id) {
-    setQuestions((prevQuestions) => prevQuestions.filter((quiz) => quiz.id !== id));
-  }
-
-  function handleUpdate(id, newCorrectIndex) {
-    setQuestions((prevQuestions) =>
-      prevQuestions.map((quiz) =>
-        quiz.id === id ? { ...quiz, correctIndex: newCorrectIndex } : quiz
-      )
-    );
-  }
-
+function QuestionList({ questions, removeQuestion }) {
   return (
     <section>
-      <h1>Question List</h1>
-      <NewQuestionForm onAddQuestion={handleAddQuestion} />
+      <h1>Quiz Questions</h1>
       <ul>
         {questions.map((question) => (
           <QuestionItem
             key={question.id}
             question={question}
-            onDelete={handleDelete}
-            onUpdate={handleUpdate}
+            removeQuestion={removeQuestion} 
           />
         ))}
       </ul>
     </section>
   );
 }
-
 
 export default QuestionList;
